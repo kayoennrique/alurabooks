ca8c8b9e538414e498515c8af4b101bb1b70d055
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-27 20:25:02 -0300

    Book MiniCard

---
+++ src/components/CardBook/index.tsx
@@ -0,0 +1,36 @@
+import { AbButton } from "alurabooks-ds-develop";
+import { Link } from "react-router-dom";
+import { IBook } from "../../interfaces/IBooks";
+import { formatter } from "../../utils/currency-formatter.ts";
+
+import './CardBook.css'
+
+
+interface CardBookProps {
+    book: IBook
+}
+
+const getMinimumValue = (book: IBook) => {
+    return Math.min(...book.optionsPurchase.map(op => op.price));
+}
+
+const CardBook = ( { book } : CardBookProps) => {
+    return (<div className="book" key={book.id}>
+        <img src={book.imageCover} alt={book.description} />
+        <ul>
+            <li>
+                <strong>{book.title}</strong>
+            </li>
+            <li>
+                A partir de: <strong>{formatter.format(getMinimumValue(book))}</strong>
+            </li>
+            <li className="link-container">
+                <Link to={`/book/${book.slug}`}>
+                    <AbButton isBlock text="Comprar" />
+                </Link>
+            </li>
+        </ul>
+    </div>);
+}
+
+export default CardBook;
