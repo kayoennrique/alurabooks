f4ce42590aa805500fca6d69f3852066e7e756d7
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-29 17:00:33 -0300

    Creating Search

--- src/components/BooksList/index.tsx
+++ src/components/BooksList/index.tsx
@@ -4,33 +4,58 @@ import CardBook from "../CardBook";
 
 import './BooksList.css'
 import { IBook } from "../../interfaces/IBooks";
+import { useState } from "react";
+import { AbButton, AbTextField } from "alurabooks-ds-develop";
 
 interface BooksListProps {
     categorie: ICategorie
 }
 
 const GET_BOOKS = gql`
-    query GetBooks {
-        books {
-            id
-            slug
-            title
-            imageCover
-            optionPurchase {
-                id
-                price
-        }
-        }
+query GetBooks($categorieId: Int, $title: String) {
+    books(categorieId: $categorieId, title: $title) {
+      id
+      slug
+      title
+      imageCover
+      optionPurchase {
+        id
+        price
+      }
     }
+  }
 `;
 
 const BooksList = ({ categorie }: BooksListProps) => {
 
-    const { data } = useQuery<{ books: IBook[] }>(GET_BOOKS)
-    // const { data: products } = useQuery(['searchOfBooksByCategory', categorie], () => getProductsFromCategory(categorie))
+    const [textSearch, setTextSearch] = useState('');
 
-    return <section className="books">
-        {data?.books.map(book => <CardBook book={book} key={book.id} />)}
+    const { data, refetch } = useQuery<{ books: IBook[]}>(GET_BOOKS, {
+        variables: {
+            categorieId: categorie.id
+        }
+    })
+
+    const searchBooks = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (textSearch) {
+            refetch({
+                categorieId: categorie.id,
+                title: textSearch
+            })
+        }
+    }
+
+    return <section>
+        <form onSubmit={searchBooks} style={{ maxWidth: '80%', margin: '0 auto', textAlign: 'center' }}>
+            <AbTextField value={textSearch} onChange={setTextSearch} placeholder='Digite o título'/>
+            <div style={{ marginTop: '16px' }}>
+                <AbButton text="Buscar"/>
+            </div>
+        </form>
+        <div className="books">
+            {data?.books.map(book => <CardBook book={book} key={book.id} />)}
+        </div>
     </section>
 }
 
