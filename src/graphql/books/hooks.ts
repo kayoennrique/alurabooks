2e6ae9495492f2a97f727ae9fe454455aab6846d
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-30 02:43:20 -0300

    Looking for reactivation

--- src/graphql/books/hooks.ts
+++ src/graphql/books/hooks.ts
@@ -1,19 +1,21 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useReactiveVar } from "@apollo/client";
 import { IBook } from "../../interfaces/IBooks";
 import { ICategorie } from "../../interfaces/ICategorie";
 import { GET_BOOKS } from "./queries";
-import { booksVar } from "./state";
+import { booksVar, filterBooksVar } from "./state";
 
 
 export const useBooks = (categorie: ICategorie) => {
-    return useQuery<{ books: IBook[]}>(GET_BOOKS, {
+    const filter = useReactiveVar(filterBooksVar)
+    return useQuery<{ books: IBook[] }>(GET_BOOKS, {
         variables: {
-            categorieId: categorie.id
+            categorieId: filter.categorie?.id,
+            title: filter.title
         },
         onCompleted(data) {
             if (data.books) {
                 booksVar(data.books)
             }
-        }
+        },
     })
 }
--- src/graphql/books/state.ts
+++ src/graphql/books/state.ts
@@ -1,4 +1,12 @@
 import { makeVar } from "@apollo/client";
 import { IBook } from "../../interfaces/IBooks";
+import { ICategorie } from "../../interfaces/ICategorie";
 
-export const booksVar= makeVar<IBook[]>([]);
+interface FilterBooks {
+    categorie?: ICategorie,
+    title?: string
+}
+
+export const filterBooksVar = makeVar<FilterBooks>({})
+
+export const booksVar = makeVar<IBook[]>([])
